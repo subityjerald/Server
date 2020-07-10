552b6491729e5d9fd5f3b3e4c5ced82eb5053529
subity jerald <subityjerald1994@gmail.com>
2020-07-10 20:16:16 +0530

    twonum

---
+++ src/Add.java
@@ -0,0 +1,17 @@
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Add extends HttpServlet
+{
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		int i=Integer.parseInt( req.getParameter("num1"));
+		int j=Integer.parseInt( req.getParameter("num2"));
+		int k=i+j;
+		res.getWriter().println("The value is :" + k);
+	}
+}
