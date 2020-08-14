72347f15844d3d8e2cad38613473e9acc44b0d2f
subity jerald <subityjerald1994@gmail.com>
2020-08-14 17:59:37 +0530

    send redirect

--- send redirect/src/Sendredirect.java
+++ send redirect/src/Sendredirect.java
@@ -1,19 +1,16 @@
-import java.io.PrintWriter;
-
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class rewritting2 extends HttpServlet {
+
+public class Sendredirect extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest req,HttpServletResponse res) {
 		res.setContentType("text/html");
 		try {
-		final PrintWriter a=res.getWriter();
-		final String usern=req.getParameter("username");
-		final String userp=req.getParameter("password");
-		a.println(usern);
-		a.println(userp);
+
+				final String b=req.getParameter("uname");
+				res.sendRedirect("https://www.amazon.co.in/#q"+ b);
 		}catch(final Exception e) {
 			e.printStackTrace();
 		}
